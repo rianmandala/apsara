895cd1f44a6da5201adb4c27786b89d9f90abc21
rianmandala <rian.putra01@gojek.com>
2025-09-15 15:19:04 +0700

    chore: add support custom classname and inline style drawer component

--- packages/apsara-ui/src/Drawer/Drawer.stories.tsx
+++ packages/apsara-ui/src/Drawer/Drawer.stories.tsx
@@ -1,21 +1,22 @@
-import { useState } from 'react';
-import Drawer from './Drawer';
+import { useState } from "react";
+import Drawer from "./Drawer";
 
 export const Basic = () => {
     const [open, setOpen] = useState(false);
     return (
         <>
-            <button type="button" onClick={() => setOpen(true)}>Click to open Drawer</button>
-            <Drawer
-                open={open}
-                onClose={() => setOpen(false)}
-            >
+            <button type="button" onClick={() => setOpen(true)}>
+                Click to open Drawer
+            </button>
+            <Drawer drawerStyle={{ zIndex: 1 }} open={open} onClose={() => setOpen(false)}>
                 <p>This is sample drawer content</p>
-                <button type="button" onClick={() => console.log('test')}>Test</button>
+                <button type="button" onClick={() => console.log("test")}>
+                    Test
+                </button>
             </Drawer>
         </>
     );
-}
+};
 
 export default {
     title: "Overlay/Drawer",
